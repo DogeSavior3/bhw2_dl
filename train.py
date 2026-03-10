c1178313bd3972c157c1cbe0f8267b5c89193c3f
Michael Korol <mvkorol@edu.hse.ru>
2026-03-10 19:42:04 +0300

    lol

--- train.py
+++ train.py
@@ -9,18 +9,19 @@ from tqdm.notebook import tqdm
 def plot_losses(train_losses, val_losses):
     plt.figure(figsize=(10,6))
     plt.plot(range(1, len(train_losses) + 1), train_losses, label = 'train')
-    plt.plot(range(1, len(val_losses) + 1), val_losses, label = 'train')
+    plt.plot(range(1, len(val_losses) + 1), val_losses, label = 'val')
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.grid(True)
     plt.tight_layout()
+    plt.legend()
     plt.show()
 
 def train_epoch(model, optimizer, criterion, loader, device, pad_ind):
     model.train()
     epoch_loss = 0.0
 
-    for source, target, source_len, target_len in tqdm(loader, desc='batch:'):
+    for source, target, source_len, target_len in tqdm(loader, desc='train', leave=True):
         source = source.to(device)
         target = target.to(device)
         source_len = source_len.to(device)
@@ -31,7 +32,6 @@ def train_epoch(model, optimizer, criterion, loader, device, pad_ind):
 
         source_pad_mask = source == pad_ind
         target_pad_mask = target_input == pad_ind
-
         optimizer.zero_grad()
         logits = model(source, target_input, source_pad_mask, target_pad_mask)
 
@@ -48,11 +48,11 @@ def train_epoch(model, optimizer, criterion, loader, device, pad_ind):
     return epoch_loss / len(loader.dataset)
 
 @torch.no_grad()
-def val_epoch(model, optimizer, criterion, loader, device):
+def val_epoch(model, optimizer, criterion, loader, device, pad_ind):
     model.eval()
     epoch_loss = 0.0
 
-    for source, target, source_len, target_len in tqdm(loader, desc='batch:', position=0, leave=False):
+    for source, target, source_len, target_len in tqdm(loader, desc='val', leave=True):
         source = source.to(device)
         target = target.to(device)
         source_len = source_len.to(device)
@@ -60,10 +60,10 @@ def val_epoch(model, optimizer, criterion, loader, device):
 
         target_input = target[:, :-1]
         target_output = target[:, 1:]
-        seq_len = target_input.size(1)
-        target_mask = torch.arange(seq_len, device=device)[None, :] < (target_len - 1)[:, None]
+        source_pad_mask = source == pad_ind
+        target_pad_mask = target_input == pad_ind
 
-        logits = model(source, target_input, source_len, target_mask)
+        logits = model(source, target_input, source_pad_mask, target_pad_mask)
 
         logits_flat = logits.reshape(-1, logits.size(-1))
         target_flat = target_output.reshape(-1)
@@ -78,10 +78,10 @@ def train(model, optimizer, train_loader, val_loader, num_epochs, device, pad_in
     train_losses = []
     val_losses = []
 
-    for epochs in tqdm(range(1, num_epochs + 1), desc='Epoch: ', position=1):
+    for epochs in tqdm(range(1, num_epochs + 1), desc='Epoch'):
         train_loss = train_epoch(model, optimizer, criterion, train_loader, device, pad_ind)
         train_losses.append(train_loss)
-        val_loss = val_epoch(model, optimizer, criterion, train_loader, device)
+        val_loss = val_epoch(model, optimizer, criterion, train_loader, device, pad_ind)
         val_losses.append(val_loss)
 
         plot_losses(train_losses, val_losses)

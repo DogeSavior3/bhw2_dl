f09f5a78b16feb547041ec63463b9cc6f1231b7c
Michael Korol <mvkorol@edu.hse.ru>
2026-03-11 13:44:44 +0300

    added AMP

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from IPython.display import clear_output
 from tqdm.notebook import tqdm
 from dataset import Vocabulary
 from model import Transformer
+from torch.cuda.amp import autocast, GradScaler
 
 def plot_losses(train_losses, val_losses):
     plt.figure(figsize=(10,6))
@@ -19,7 +20,7 @@ def plot_losses(train_losses, val_losses):
     plt.legend()
     plt.show()
 
-def train_epoch(model, optimizer, criterion, loader, device, pad_ind):
+def train_epoch(model, optimizer, criterion, loader, device, pad_ind, scaler):
     model.train()
     epoch_loss = 0.0
 
@@ -35,16 +36,19 @@ def train_epoch(model, optimizer, criterion, loader, device, pad_ind):
         source_pad_mask = (source == pad_ind).to(device)
         target_pad_mask = (target_input == pad_ind).to(device)
         optimizer.zero_grad()
-        logits = model(source, target_input, source_pad_mask, target_pad_mask)
 
-        logits_flat = logits.reshape(-1, logits.size(-1))
-        target_flat = target_output.reshape(-1)
-
-        loss = criterion(logits_flat, target_flat)
-        loss.backward()
-
+        with autocast():
+            logits = model(source, target_input, source_pad_mask, target_pad_mask)
+            logits_flat = logits.reshape(-1, logits.size(-1))
+            target_flat = target_output.reshape(-1)
+            loss = criterion(logits_flat, target_flat)
+        
+        scaler.scale(loss).backward()
+        scaler.unscale_(optimizer)
         nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        optimizer.step()
+        # optimizer.step()
+        scaler.step(optimizer)
+        scaler.update()
         epoch_loss += loss.item() * source.size(0)
 
     return epoch_loss / len(loader.dataset)
@@ -64,12 +68,12 @@ def val_epoch(model, optimizer, criterion, loader, device, pad_ind):
         target_output = target[:, 1:]
         source_pad_mask = (source == pad_ind).to(device)
         target_pad_mask = (target_input == pad_ind).to(device)
+        with autocast():
+            logits = model(source, target_input, source_pad_mask, target_pad_mask)
 
-        logits = model(source, target_input, source_pad_mask, target_pad_mask)
-
-        logits_flat = logits.reshape(-1, logits.size(-1))
-        target_flat = target_output.reshape(-1)
-        loss = criterion(logits_flat, target_flat)
+            logits_flat = logits.reshape(-1, logits.size(-1))
+            target_flat = target_output.reshape(-1)
+            loss = criterion(logits_flat, target_flat)
 
         epoch_loss += loss.item() * source.size(0)
     
@@ -80,9 +84,10 @@ def train(model, optimizer, train_loader, val_loader, num_epochs, device, pad_in
     train_losses = []
     val_losses = []
     best_loss = float('inf')
+    scaler = GradScaler()
 
     for epochs in tqdm(range(1, num_epochs + 1), desc='Epoch'):
-        train_loss = train_epoch(model, optimizer, criterion, train_loader, device, pad_ind)
+        train_loss = train_epoch(model, optimizer, criterion, train_loader, device, pad_ind, scaler)
         train_losses.append(train_loss)
         val_loss = val_epoch(model, optimizer, criterion, val_loader, device, pad_ind)
         val_losses.append(val_loss)

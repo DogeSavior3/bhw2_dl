0613bfe2f16781cccb226fefe9c50511af160f77
Michael Korol <mvkorol@edu.hse.ru>
2026-03-11 10:35:48 +0300

    added device

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         position = torch.arange(max_len, device=device).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2, device=device) * (-math.log(10000.0) / d_model))
         pe = torch.zeros(max_len, d_model, device=device)
         pe[:, 0::2] = torch.sin(position * div_term, device=device)
         pe[:, 1::2] = torch.cos(position * div_term, device=device)
